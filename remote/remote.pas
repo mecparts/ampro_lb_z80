a120c800e2e78273b3b545db563e06044f7c5912
mecparts <2958269+mecparts@users.noreply.github.com>
2021-11-15 13:37:59 -0700

    Status updates added to REMOTE

--- remote/remote.pas
+++ remote/remote.pas
@@ -1,3 +1,20 @@
+{
+  REMOTE: swaps logical CONsole between physical CRT
+  and TTY. Assumes my Retro Wifi Modem is connected
+  to Aux and is set up to allow incoming connections.
+
+  When switching from CRT: to TTY: the port is set up
+  for 38.4K and the Wifi modem is configured for
+  silent running (echo off, quiet on).
+
+  When switching back from TTY: to CRT:, the modem is
+  commanded to return to local command mode (+++) and
+  then echo is turned back on and quiet off before
+  the modem is command to hang up, ending the telnet
+  connection.
+
+  Nov 15/21 WH
+}
 PROGRAM Remote;
 
 CONST
@@ -13,6 +30,10 @@ VAR
 {$I MDMINRDY}
 {$I OFFLINE }
 
+{
+  consume any characters arriving from the serial port
+  until 500ms of silence is seen
+}
 PROCEDURE Purge;
 VAR
   timeout : INTEGER;
@@ -30,6 +51,7 @@ BEGIN
   UNTIL timeOut < 0;
 END;
 
+{ return BYE compatible MSPEED value }
 FUNCTION BaudIndex(baud : INTEGER) : BYTE;
 BEGIN
   CASE baud OF
@@ -48,6 +70,7 @@ BEGIN
   END;
 END;
 
+{ set serial port and Wifi modem to requested baud }
 PROCEDURE SetTTY(baud: INTEGER);
 CONST
   BIOSioinit = 18; { Ampro extended BIOS call to init I/O system }
@@ -79,25 +102,30 @@ CONST
 
 BEGIN
   IF (Mem[IObyte] AND $03) = 0 THEN BEGIN
-    Mem[IObyte] := Mem[IObyte] OR $01;
+    { Console is TTY:, switch to CRT: }
+    WriteLn('Switching console to CRT:');
+    Mem[IObyte] := Mem[IObyte] OR $01; { IObyte CON:=CRT: }
     Delay(1100);
-    Write(Aux,attnStr);
+    Write(Aux,attnStr);                { Wifi modem to command mode }
     Delay(1100);
     Purge;
     Write(Aux, CR);
     Purge;
-    Write(Aux, activeStr, CR);
+    Write(Aux, activeStr, CR);         { Turn echo back on }
     Purge;
-    Write(Aux, hangupStr, CR);
+    Write(Aux, hangupStr, CR);         { End telnet connection }
     Purge;
   END ELSE BEGIN
+    { Assume console is CRT:, switch to TTY: }
+    { Check for expected response to AT<CR>  }
     IF NOT IsOffline THEN BEGIN
       WriteLN('Serial port is busy.');
       HALT;
     END;
     SetTTY(9600);
-    Write(Aux, quietStr, CR);
+    Write(Aux, quietStr, CR);          { Turn echo off }
     Purge;
+    WriteLn('Switching console to TTY:');
     Mem[IObyte] := (Mem[IObyte] AND $FC);
   END;
 END.
